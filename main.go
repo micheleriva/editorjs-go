f837398b418d5bb987301b647c5a623ad232f7d2
Michele Riva <master@micheleriva.it>
2020-09-02 23:20:02 +0200

    fix: eliminated dead code

--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-)
-
 func main() {
-	data, err := ioutil.ReadFile("./tests/mocks/mock1.json")
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	result := Markdown(string(data))
-	fmt.Println(result)
 }
